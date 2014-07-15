da5aca69d843c70dac37fd283dfd5ba4e3edfdd8
ppd225 <ppd225@nyu.edu>
2014-07-15 16:18:16 -0400

    Update follows.js

--- follows.js
+++ follows.js
@@ -17,7 +17,7 @@
 		applicationName	= 'Parth\'s Application', // Replaces Xively logo in the header
 		dataDuration	= '60days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
+		dataColor		= '9AC725', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
@@ -137,7 +137,7 @@
 									series.push({
 										name: datastream.id,
 										data: points,
-										color: '#' + dataColor
+										color: '#13359c'
 									});
 
 									// Initialize Graph DOM Element
